c35a5dad496468907b5d87a53520993f0db662df
YellowTail <hwhaocool@gmail.com>
2019-01-03 23:43:18 +0800

    impl Condition logic

--- src/main/java/com/github/hwhaocool/yellow/validate/ValidateTool.java
+++ src/main/java/com/github/hwhaocool/yellow/validate/ValidateTool.java
@@ -1,5 +1,11 @@
 package com.github.hwhaocool.yellow.validate;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class ValidateTool {
 	
 	private static final ValidateTool  DEFAULT_INSTANCE;
@@ -7,34 +13,36 @@ public class ValidateTool {
       DEFAULT_INSTANCE = new ValidateTool();
     }
 
-	public static Builder newBuilder() {
-        return DEFAULT_INSTANCE.toBuilder();
+	public static Condition newCondition() {
+        return DEFAULT_INSTANCE.toCondition();
       }
     
-    private Builder toBuilder() {
-        return new Builder();
+    private Condition toCondition() {
+        return new Condition();
     }
-
-    public static final class Builder {
-    	private boolean required = false;
+    
+    public static final class Condition {
+    	private Builder validateData;
     	
-    	private int minLength = 0;
+    	public Condition() {
+    		this.validateData = new Builder();
+    	}
     	
-    	private int maxLength = 0;
-    	
-    	private String regexPattern = null;
-    	
-    	public Builder required() {
+    	public Condition required() {
     		return required(true);
     	}
     	
-    	public Builder required(boolean value) {
-    		this.required = value;
+    	public Condition required(boolean value) {
+    		validateData.required = value;
     		return this;
     	}
     	
-    	public Builder regex(String value) {
-    		this.regexPattern = value;
+    	public Condition regex(String value) {
+    		if (StringUtils.isEmpty(value)) {
+				throw new IllegalArgumentException("regex pattern can't be empty");
+			}
+    		
+    		validateData.regexPattern = value;
     		return this;
     	}
     	
@@ -47,12 +55,65 @@ public class ValidateTool {
     	 * @author hwhaocool
     	 * @return
     	 */
-    	public Builder length(String description) {
+    	public Condition length(String description) {
+    		if (StringUtils.isEmpty(description)) {
+				throw new IllegalArgumentException("length description can't be empty");
+			}
+    		
+    		validateData.lengthDesc = description;
     		return this;
     	}
     	
+    	public Validate from(String value) {
+    		return fromValue(value);
+    	}
+    	
+    	@SuppressWarnings("rawtypes")
+		public Validate from(Collection value) {
+    		return fromValue(value);
+    	}
+    	
+    	@SuppressWarnings("rawtypes")
+		public Validate from(Map value) {
+    		return fromValue(value);
+    	}
+    	
+    	private Validate fromValue(Object value) {
+    		return new Validate(validateData, value);
+    	}
+    }
+
+    @SuppressWarnings("unused")
+    private static final class Builder {
+    	
+		private boolean required = false;
+    	
+    	private String lengthDesc;
+    	
+    	private Length maxLength;
+    	
+    	private String regexPattern = null;
+    }
+    
+    public static final class Validate {
+    	private Builder validateData;
+    	
+    	private Object value;
+    	
+    	private Pattern lengthAnalysisPattern = Pattern.compile("");
+    	
+    	public Validate(Builder builder, Object value) {
+			this.validateData = builder;
+			this.value = value;
+		}
+    	
     	public void validate(RuntimeException ex) {
-    		throw ex;
+    		boolean pass = validateValue(value);
+    		if (! pass) {
+    			throw ex;
+			} else {
+				//do nothing
+			}
     	}
     	
     	public void validate(String msg) {
@@ -62,5 +123,45 @@ public class ValidateTool {
     	public void validate() {
     		validate(new IllegalArgumentException());
     	}
+    	
+    	private boolean validateValue(Object value) {
+    		if (null == value) {
+				if (validateData.required) {
+					return false;
+				}
+				return true;
+			}
+    		
+    		//analysis length description
+    		
+    		
+    		if (value instanceof String) {
+				return validateString((String) value);
+			}
+    		return true;
+    	}
+    	
+    	private boolean validateString(String str) {
+    		if (true) {
+				
+			}
+    		return true;
+    	}
+    }
+    
+    private static final class Length {
+    	private int value;
+    	private boolean canEquals;
+    	
+    	public Length(int value, boolean canEquals) {
+    		this.value = value;
+    		this.canEquals= canEquals;
+    	}
+
+		@Override
+		public String toString() {
+			return "Length [value=" + value + ", canEquals=" + canEquals + "]";
+		}
+    	
     }
 }
